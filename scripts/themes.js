96c65de8f1424b9e5f52fe9c0ae6c92ce4bb5d64
Giovanni <gipralon@users.noreply.github.com>
2021-01-07 11:36:50 -0200

    change themes icon and position.

--- scripts/themes.js
+++ scripts/themes.js
@@ -3,7 +3,8 @@ const body = document.querySelector('body');
 const title = document.querySelector('.inputBox h2');
 const btn = document.querySelector('.inputBox #btn');
 const password = document.querySelector('.inputBox input#password');
-const source = document.querySelector('.source a');
+//const source = document.querySelector('.source a');
+const themesImg = document.querySelector('footer img .themes');
 
 function themes(){
   console.log('click.');
@@ -24,9 +25,9 @@ function dark(){
   password.style.border = "2px solid rgba(0,0,0,0.4)";
   password.style.boxShadow = "none";
   //source.style.color = "grey";
-  theme.style.backgroundColor = "#f8f8f8";
-  theme.style.color = "#333";
-  theme.innerText = "Light Mode";
+  //theme.style.backgroundColor = "#f8f8f8";
+  //theme.style.color = "#333";
+  //theme.innerText = "Light Mode";
   body.style.transitionDelay = "0.3s";
   body.style.transitionDuration = "1s";
   console.log('dark mode successfully applied.');
@@ -38,9 +39,9 @@ function light(){
   title.removeAttribute('style');
   btn.removeAttribute('style');
   password.removeAttribute('style');
-  theme.removeAttribute('style');
-  theme.removeAttribute('innerText');
-  theme.innerText = "Dark Mode";
+  //theme.removeAttribute('style');
+  //theme.removeAttribute('innerText');
+  //theme.innerText = "Dark Mode";
   //source.removeAttribute('style');
   body.style.transitionDelay = "0.3s";
   body.style.transitionDuration = "1s"
